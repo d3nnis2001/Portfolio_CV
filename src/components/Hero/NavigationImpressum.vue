f6d5189af6d4bc0cfb51ef2f130762f379c4acfd
Descy <d3nnis.s@web.de>
2024-09-29 03:36:22 +0200

    added final touch to hero. Only Picture is missing

--- src/components/Hero/NavigationImpressum.vue
+++ src/components/Hero/NavigationImpressum.vue
@@ -2,7 +2,7 @@
   <div class="flex items-center space-x-4">
     <span class="text-[25px] font-extrabold text-orange-500">Impressum</span>
     <div class="w-[3px] h-16 bg-white bg-opacity-25 rounded-full"></div>
-    <div class="grid grid-cols-3 gap-2">
+    <div class="grid grid-cols-3 gap-4">
       <div v-for="dot in 6" :key="dot"
            :class="[
              'w-4 h-4 rounded-full',
--- src/views/Hero.vue
+++ src/views/Hero.vue
@@ -1,23 +1,24 @@
 <template>
-  <div class="flex flex-col">
-    <Navbar class="mt-5 mb-24 "/>
-    <div class="flex flex-row justify-center">
-      <div class="flex flex-col">
-        <TextPart/>
-        <Socials/>
-        <div class="flex flex-row items-center mt-14">
-          <ContactButton class="mr-8"/>
-          <DownloadCVButton/>
+  <div class="flex flex-col min-h-screen">
+    <Navbar class="mt-5 mb-24" />
+    <div class="flex-grow flex flex-col">
+      <div class="flex flex-row justify-center flex-grow">
+        <div class="flex flex-col">
+          <TextPart />
+          <Socials />
+          <div class="flex flex-row items-center mt-14">
+            <ContactButton class="mr-8" />
+            <DownloadCVButton />
+          </div>
         </div>
+        <Picture class="ml-10" />
       </div>
-      <Picture class="ml-10"/>
+      <NavigationImpressum class="mt-auto mb-8 flex justify-center" :filledDots="[1]" />
     </div>
-    <NavigationImpressum class="flex justify-center" :filledDots="[1]"/>
   </div>
 </template>
 
 <script setup>
-
 import Navbar from "@/components/Hero/Navbar.vue";
 import DownloadCVButton from "@/components/Hero/DownloadCVButton.vue";
 import Picture from "@/components/Hero/Picture.vue";
@@ -25,9 +26,7 @@ import Socials from "@/components/Hero/Socials.vue"
 import TextPart from "@/components/Hero/TextPart.vue";
 import ContactButton from "@/components/Hero/ContactButton.vue";
 import NavigationImpressum from "@/components/Hero/NavigationImpressum.vue";
-
 </script>
 
 <style>
-
 </style>
