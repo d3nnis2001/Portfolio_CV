3f09f7ded2c782b780bcb3241a1c2beb4a738e79
Descy <d3nnis.s@web.de>
2024-11-02 01:27:44 +0100

    added some new stuff to about2 page

--- src/components/About/WorkCard.vue
+++ src/components/About/WorkCard.vue
@@ -1,11 +1,32 @@
-<script setup>
-
+<template>
+  <div class="flex flex-col text-start justify-around">
+    <h2 class="text-2xl color-orange font-bold mb-2">{{year}}</h2>
+    <h3 class="text-xl text-white/80 font-semibold mb-1">{{heading}}</h3>
+    <p class="text-base text-white/60 font-semibold">{{text}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'About2',
+  props: {
+    year: {
+      type: String,
+      required: true
+    },
+    heading: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    }
+  }
+}
 </script>
 
-<template>
-
-</template>
-
 <style scoped>
-
+.color-orange {
+  color: #FD6F00;
+}
 </style>
--- src/views/About2.vue
+++ src/views/About2.vue
@@ -1,11 +1,38 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <div class="flex flex-col h-screen w-screen">
+    <div class="flex flex-row h-3/4">
+      <div class="flex flex-col justify-around w-1/2 items-center text-start">
+        <h1 class="text-3xl font-extrabold text-white/80">Education</h1>
+        <div v-for="(item, index) in education" :key="index">
+          <WorkCard :text="item.desc" :heading="item.name" :year="item.year" />
+        </div>
+        <h1 class="text-3xl font-extrabold text-white/80">Work</h1>
+        <div v-for="(item, index) in work" :key="index">
+          <WorkCard :text="item.desc" :heading="item.name" :year="item.year" />
+        </div>
+      </div>
+      <HobbyCard class="w-1/2"/>
+    </div>
+    <NavigationImpressum :filled-dots="[3]" class="mb-8"/>
+  </div>
 </template>
 
-<style scoped>
+<script setup>
+import WorkCard from "@/components/About/WorkCard.vue";
+import HobbyCard from "@/components/About/HobbyCard.vue";
+import NavigationImpressum from "@/components/Hero/NavigationImpressum.vue";
 
+const education = [
+  {year: "2018-2021", name: "Abitur", desc: "Math and Computer Science | CSBWV"},
+  {year: "2021-2025", name: "Bachelor of Science", desc: "Cognitive Computer Science | Universität Bielefeld"}
+]
+
+const work = [
+  {year: "2019-2022", name: "Part-time Job  ", desc: "Marktkauf Oldentrup"},
+  {year: "2022-Today", name: "Technical Consultant", desc: "Working Student | Triggeryou GmbH"}
+];
+</script>
+
+<style scoped>
+/* Deine Styles hier */
 </style>
