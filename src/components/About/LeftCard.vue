6bec7bc3b91430b96deaf998f96c27a5c72a1d10
d3nnis2001 <d3nnis.s@web.de>
2025-02-23 17:00:58 +0100

    changed site icon and name

--- src/components/About/LeftCard.vue
+++ src/components/About/LeftCard.vue
@@ -6,15 +6,15 @@
     <div class="flex flex-row">
       <div class="color-bg-orange w-3 rounded-lg mr-4 fade-flow"></div>
       <div class="flex flex-col">
-        <p class="text-white/80 mb-10 text-xl lg:text-3xl xl:text-4xl">
+        <p class="text-white/80 mb-6 lg:mb-10 text-lg lg:text-3xl xl:text-4xl">
           Hi, I’m Dennis – an aspiring AI enthusiast and software engineer.
           I recently graduated from university and am now actively looking for
           opportunities in
-          <span class="text-xl lg:text-3xl xl:text-4xl color-orange font-semibold">
+          <span class="text-lg lg:text-3xl xl:text-4xl color-orange font-semibold">
             AI, Data Science, or Software Engineering
           </span>
         </p>
-        <p class="text-white/80 text-xl lg:text-3xl xl:text-4xl">
+        <p class="text-white/80 text-lg lg:text-3xl xl:text-4xl">
           Currently, I’m open to exciting job opportunities and collaborations.
           Feel free to connect with me!
         </p>
--- src/components/Hero/Picture.vue
+++ src/components/Hero/Picture.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative w-[250px] h-[250px] lg:w-[450px] lg:h-[450px]">
+  <div class="relative w-[200px] h-[200px] lg:w-[450px] lg:h-[450px]">
     <div class="absolute inset-0 rounded-full bg-[#D9D9D9] bg-opacity-5"></div>
     <img
         src="../icons/PictureMe.png"
         alt="Me"
-        class="absolute w-[350px]  lg:w-[650px]"
+        class="absolute w-[270px]  lg:w-[650px]"
         style="top: 38%; left: 45%; transform: translate(-50%, -50%); height: auto; max-width: none; opacity: 0.85;"
     />
   </div>
